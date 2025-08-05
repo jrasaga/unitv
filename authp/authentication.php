f68d3584c19842c78aa2d6e7b2e86934860a90c1
jrasaga <65923110+jrasaga@users.noreply.github.com>
2025-08-04 23:56:33 -0300

    Update authentication.php

--- authp/authentication.php
+++ authp/authentication.php
@@ -1,11 +1,10 @@
-<?php
-include 'AES256.php';
+'httpsinclude 'AES256.php';
 
 $passphrase = "ertytredcvfbgyhtfrfgtnytgfnvbngmhjgnf";
 
 $responsh = $_GET['e'];
 $aa = \mervick\aesEverywhere\AES256::decrypt($responsh, $passphrase);
-$ab = file_get_contents('https://SeuPainelAqui/htv/api/authentication.php?e='.$aa);
+$ab = file_get_contents('https://github.com/jrasaga/unitv/htv/api/authentication.php?e='.$aa);
 echo $ab;
 
 //Developer STLC
